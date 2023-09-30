7ac87683f7ddeadd532d716431a62b9ef44d32d2
khalif <khalif.cooper@outlook.com>
2023-09-30 14:40:02 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Khalif Cooper on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
